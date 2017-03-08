b1076f35f8bafcc4694d4dc2475ea1050c5a2708
Ruslan Primak <ruslan.primak@gmail.com>
2017-03-08 19:20:32 +0200

    Applying views binding via DataBindingUtil

--- app/src/main/java/com/example/android/scorekeeper/TeamFragment.java
+++ app/src/main/java/com/example/android/scorekeeper/TeamFragment.java
@@ -9,6 +9,7 @@ package com.example.android.scorekeeper;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -20,10 +21,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.android.scorekeeper.databinding.FragmentTeamBinding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,7 +44,7 @@ public class TeamFragment extends Fragment {
     private final static int EP_CANCEL = 0;     // Points for ExtraPoints Cancel button
 
     // STATE KEYS
-    private final static String KEY_EXG_VISIBILITY = "EXG_VISIBILITY";    // Extra Point View Group visibility
+    private final static String KEY_EXG_VISIBILITY = "EXG_VISIBILITY"; // Extra Point View Group visibility
     private final static String KEY_SCORE_POINTS_VALUE = "SCORE_POINTS_VALUE"; // Value of score point
     private final static String KEY_CURRENT_TEAM = "CURRENT_TEAM"; // Selected team
     private final static String KEY_FORBIDDEN_TEAM = "FORBIDDEN_TEAM"; // Team forbidden for selection
@@ -56,24 +56,22 @@ public class TeamFragment extends Fragment {
 
     private int mScorePoints = 0; // Score for a team
 
-    private Spinner mTeamSpinner; // reference to the team spinner
-    private TextView mTextViewScore; // reference to the Score TextView
-    private LinearLayout mLayoutScorePoints; // reference to the Layout with Score Points controls
-    private LinearLayout mLayoutExtraPoints; // reference to the Layout with ExtraPoint controls
-    private Button mButtonTD; // reference to the touchdown button
     private FragmentButtonOnClickListener mListener; // reference to listener of this fragment events
+
+    private FragmentTeamBinding mBinding; // fragment's binding
 
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
 
+        // checking parent activity to implement FragmentButtonOnClickListener interface
         if (context instanceof FragmentButtonOnClickListener) {
             mListener = (FragmentButtonOnClickListener) context;
         } else {
-            throw new ClassCastException(context.toString() + " must implement ScoreButtonOnClickListener.");
+            throw new ClassCastException(context.toString() +
+                    " must implement ScoreButtonOnClickListener.");
         }
-
 
         // retrieve resource IDs fro drawables if needed
         initDrawableIDs();
@@ -82,23 +80,20 @@ public class TeamFragment extends Fragment {
     /**
      * Method sets OnClickListenter for button is responsible for adding score points
      *
-     * @param container - view contains the button
-     * @param btnID     - resource id of the button
-     * @param points    - specified points to be added on button click event
+     * @param button - button listener to be applied to
+     * @param points - specified points to be added on button click event
      */
-    private void setScoreButtonOnClickListener(final View container, final int btnID,
-                                               final int points) {
-        Button button = (Button) container.findViewById(btnID);
+    private void setScoreButtonOnClickListener(final Button button, final int points) {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Score changes enabled only if a team is selected
-                if (mTeamSpinner.getSelectedItemPosition() > 0) {
+                if (mBinding.spinnerTeam.getSelectedItemPosition() > 0) {
                     updateScorePoints(mScorePoints + points);
                     mListener.onButtonClick(view);
                 } else {
-                    Toast.makeText(container.getContext(), R.string.toast_team_not_selected,
-                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mBinding.getRoot().getContext(),
+                            R.string.toast_team_not_selected, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -116,28 +111,15 @@ public class TeamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_team, container, false);
-
-        // get reference to the implementation of the text_view_score
-        mTextViewScore = (TextView) view.findViewById(R.id.text_view_score);
-
-        // get reference to the layout contains Extra Points control
-        mLayoutScorePoints = (LinearLayout) view.findViewById(R.id.layoutMainButtons);
-
-        // get reference to the layout contains Extra Points control
-        mLayoutExtraPoints = (LinearLayout) view.findViewById(R.id.layoutExtraPoints);
-
-        // get reference to the button is responsible for touchdown score
-        mButtonTD = (Button) view.findViewById(R.id.btnTouchdown);
+        // Inflate the layout binding for this fragment
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_team, container, false);
 
         // Apply the adapter to the spinner
         final TeamSpinnerAdapter teamListAdapter = new TeamSpinnerAdapter(getActivity(),
                 android.R.layout.simple_spinner_item, mTeamLogoList);
 
-        mTeamSpinner = (Spinner) view.findViewById(R.id.spinner_team);
-        mTeamSpinner.setAdapter(teamListAdapter);
-        mTeamSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        mBinding.spinnerTeam.setAdapter(teamListAdapter);
+        mBinding.spinnerTeam.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -151,14 +133,14 @@ public class TeamFragment extends Fragment {
         });
 
         // populate the buttons onClick methods
-        setScoreButtonOnClickListener(view, R.id.btnTouchdown, TD_POINTS);
-        setScoreButtonOnClickListener(view, R.id.btnFieldGoal, FG_POINTS);
-        setScoreButtonOnClickListener(view, R.id.btnSafety, SF_POINTS);
-        setScoreButtonOnClickListener(view, R.id.btnExtraPointGoal, EPG_POINTS);
-        setScoreButtonOnClickListener(view, R.id.btnExtraPointTouchdown, EPTD_POINTS);
-        setScoreButtonOnClickListener(view, R.id.btnExtraPointCancel, EP_CANCEL);
+        setScoreButtonOnClickListener(mBinding.btnTouchdown, TD_POINTS);
+        setScoreButtonOnClickListener(mBinding.btnFieldGoal, FG_POINTS);
+        setScoreButtonOnClickListener(mBinding.btnSafety, SF_POINTS);
+        setScoreButtonOnClickListener(mBinding.btnExtraPointGoal, EPG_POINTS);
+        setScoreButtonOnClickListener(mBinding.btnExtraPointTouchdown, EPTD_POINTS);
+        setScoreButtonOnClickListener(mBinding.btnExtraPointCancel, EP_CANCEL);
 
-        return view;
+        return mBinding.getRoot();
     }
 
 
@@ -167,7 +149,7 @@ public class TeamFragment extends Fragment {
      */
     private void updateScorePoints(int points) {
         mScorePoints = points;
-        mTextViewScore.setText(String.valueOf(mScorePoints));
+        mBinding.textViewScore.setText(String.valueOf(mScorePoints));
     }
 
 
@@ -179,11 +161,11 @@ public class TeamFragment extends Fragment {
      */
     private void setPointLayoutsVisibility(boolean isVisible) {
         if (isVisible) {
-            mLayoutScorePoints.setVisibility(View.GONE);
-            mLayoutExtraPoints.setVisibility(View.VISIBLE);
+            mBinding.layoutMainButtons.setVisibility(View.GONE);
+            mBinding.layoutExtraPoints.setVisibility(View.VISIBLE);
         } else {
-            mLayoutExtraPoints.setVisibility(View.GONE);
-            mLayoutScorePoints.setVisibility(View.VISIBLE);
+            mBinding.layoutExtraPoints.setVisibility(View.GONE);
+            mBinding.layoutMainButtons.setVisibility(View.VISIBLE);
         }
     }
 
@@ -219,9 +201,9 @@ public class TeamFragment extends Fragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        outState.putInt(KEY_EXG_VISIBILITY, mLayoutExtraPoints.getVisibility());
+        outState.putInt(KEY_EXG_VISIBILITY, mBinding.layoutExtraPoints.getVisibility());
         outState.putInt(KEY_SCORE_POINTS_VALUE, mScorePoints);
-        outState.putInt(KEY_CURRENT_TEAM, mTeamSpinner.getSelectedItemPosition());
+        outState.putInt(KEY_CURRENT_TEAM, mBinding.spinnerTeam.getSelectedItemPosition());
         outState.putInt(KEY_FORBIDDEN_TEAM, mForbiddenPosition);
     }
 
@@ -240,7 +222,7 @@ public class TeamFragment extends Fragment {
 
             final int selection = savedInstanceState.getInt(KEY_CURRENT_TEAM, AdapterView.INVALID_POSITION);
             if (selection != AdapterView.INVALID_POSITION)
-                mTeamSpinner.setSelection(selection);
+                mBinding.spinnerTeam.setSelection(selection);
 
             mForbiddenPosition = savedInstanceState.getInt(KEY_FORBIDDEN_TEAM, AdapterView.INVALID_POSITION);
         }
@@ -253,7 +235,7 @@ public class TeamFragment extends Fragment {
      * @param view - which button is pressed
      */
     public void anotherButtonClicked(View view) {
-        setPointLayoutsVisibility(view == mButtonTD);
+        setPointLayoutsVisibility(view == mBinding.btnTouchdown);
     }
 
     /**
@@ -270,7 +252,7 @@ public class TeamFragment extends Fragment {
      */
     public void setForbiddenItem(int itemPosition) {
         // skip because it is supposed to this has been source
-        if (mTeamSpinner.getSelectedItemPosition() != itemPosition)
+        if (mBinding.spinnerTeam.getSelectedItemPosition() != itemPosition)
             mForbiddenPosition = itemPosition;
     }
 
@@ -354,7 +336,7 @@ public class TeamFragment extends Fragment {
             if (isDropDown) {
                 if ((position == 0) || (position == mForbiddenPosition)) // disabled items
                     view.setBackgroundColor(0x24000000);
-                else if (position == mTeamSpinner.getSelectedItemPosition()) { // selected item
+                else if (position == mBinding.spinnerTeam.getSelectedItemPosition()) { // selected item
                     view.setBackgroundColor(0x48ffff00);
                 } else {
                     view.setBackgroundColor(0xffffffff);
